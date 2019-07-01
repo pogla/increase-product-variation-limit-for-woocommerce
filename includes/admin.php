8c66bcff4e8b31d3c32ec6dcebf94074a4755501
pogla <matic.pogladic@gmail.com>
2019-07-01 17:49:55 +0200

    Initial commit

---
+++ includes/admin.php
@@ -0,0 +1,66 @@
+<?php
+
+class IncreaseProductVariationLimitAdmin {
+
+	public function __construct () {
+		add_filter( 'woocommerce_general_settings', [ $this, 'add_setting' ] );
+		add_filter( 'woocommerce_ajax_variation_threshold', [ $this, 'ajax_variation_threshold' ] );
+	}
+
+	/**
+	 * Add WooCommerce general settings section
+	 *
+	 * @param $settings
+	 *
+	 * @return array
+	 */
+	public function add_setting( $settings ) {
+		$settings[] = [
+			'title' => __( 'Variation AJAX limit', 'increase-product-variation-limit-for-woocommerce' ),
+			'type'  => 'title',
+			'desc'  => __( 'When your variable product has more than 30 variations, WooCommerce starts to use ajax to load your selected variation.', 'increase-product-variation-limit-for-woocommerce' ),
+			'id'    => IPVL_PREFIX . 'custom_variation_limit_title',
+		];
+
+		$settings[] = [
+			'title'             => __( 'Number of decimals', 'increase-product-variation-limit-for-woocommerce' ),
+			'desc'              => __( 'Set variation limit.', 'increase-product-variation-limit-for-woocommerce' ),
+			'id'                => IPVL_PREFIX . 'custom_variation_limit',
+			'css'               => 'width:50px;',
+			'default'           => '30',
+			'desc_tip'          => true,
+			'type'              => 'number',
+			'custom_attributes' => [
+				'min'  => 0,
+				'step' => 1,
+			]
+		];
+
+		$settings[] = [
+			'type' => 'sectionend',
+			'id'   => IPVL_PREFIX . 'custom_variation_limit_title',
+		];
+
+		return $settings;
+	}
+
+	/**
+	 * Set new variation threshold
+	 *
+	 * @param $limit
+	 *
+	 * @return int
+	 */
+	public function ajax_variation_threshold( $limit ) {
+	
+		$new_limit = get_option( IPVL_PREFIX . 'custom_variation_limit' );
+		
+		if ( false !== $new_limit ) {
+			return absint( $new_limit );
+		}
+		
+		return $limit;
+	}
+}
+
+new IncreaseProductVariationLimitAdmin();
